997ae6a03eb5f8956a1c873dae1bad4699985deb
deocksoo <dusol92@snu.ac.kr>
2019-02-13 07:05:03 +0900

    [ADD] Stack 자료구조의 skeleton code를 추가합니다
    구현해야 할 사항들은 다음과 같습니다.
    1. main_lab4.c의 InfixToPostfix 함수를 구현해야 합니다.
    2. stack.c 에서 ADT를 구현해야 합니다.

---
+++ week2_Stack/skeleton/Stack_skeleton.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Stack.h"
+
+// 1    ->  구현 순서
+void StackInit(Stack * pstack)
+{
+
+}
+
+// 2
+int SIsEmpty(Stack * pstack)
+{
+
+}
+
+// 3
+void SPush(Stack * pstack, Data data)
+{
+
+}
+
+// 4
+Data SPop(Stack * pstack)
+{
+
+}
+
+// 5
+Data SPeek(Stack * pstack)
+{
+
+}
+
+// 6
+void PrintStack(Stack *pstack)
+{
+
+}
