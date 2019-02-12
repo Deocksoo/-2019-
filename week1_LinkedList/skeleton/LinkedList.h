b0608c8ac879a322d3d6072791f4d9fdc9fd8e6b
deocksoo <dusol92@snu.ac.kr>
2019-02-13 07:01:50 +0900

    [ADD] LinkedList.h를 LinkedList skeleton에 추가합니다

---
+++ week1_LinkedList/skeleton/LinkedList.h
@@ -0,0 +1,35 @@
+#ifndef __LINKED_LIST_H__
+#define __LINKED_LIST_H__
+
+#define TRUE    1
+#define FALSE   0
+
+typedef int ElementType;
+
+typedef struct _node
+{
+    ElementType element;
+    struct _node *next;
+} Node;
+
+typedef struct _linkedList
+{
+    Node *head;
+    Node *cur;
+    Node *before;
+    int numOfData;
+} LinkedList;
+
+typedef LinkedList List;
+
+void InitList(List *plist);
+int IsEmpty(List *plist);
+int LCount(List *plist);
+int FindNode(List *plist, ElementType key);
+int FindPrevNode(List *plist, ElementType key);
+void LInsert(List *plist, ElementType new_element, ElementType key);
+void LDelete(List *plist, ElementType key);
+void DeleteList(List *plist);
+void PrintList(List *plist);
+
+#endif
