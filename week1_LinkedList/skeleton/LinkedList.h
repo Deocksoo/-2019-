556b3db6ca581f359aedb2a0290cd4d23cf5ff88
deocksoo <dusol92@snu.ac.kr>
2019-04-05 15:42:04 +0900

    [EDIT] LinkedList skeleton 코드 일부를 수정합니다
    FindNode 함수가 Node를 직접 리턴하게 하고, ADT 구현 파일 외부에서는 접근할 수
    없도록 하였습니다. 이로 인해 LinkedList 구조체에서 필요하지 않게 된 cur, before
    포인터를 제거하였고, numOfData도 아직은 필요 없는 기능이라 제거하였습니다.

--- week1_LinkedList/skeleton/LinkedList.h
+++ week1_LinkedList/skeleton/LinkedList.h
@@ -15,16 +15,12 @@ typedef struct _node
 typedef struct _linkedList
 {
     Node *head;
-    Node *cur;
-    Node *before;
-    int numOfData;
 } LinkedList;
 
 typedef LinkedList List;
 
 void InitList(List *plist);
 int IsEmpty(List *plist);
-int FindNode(List *plist, ElementType key);
 int FindPrevNode(List *plist, ElementType key);
 void LInsert(List *plist, ElementType new_element, ElementType key);
 void LDelete(List *plist, ElementType key);
--- week1_LinkedList/skeleton/LinkedList_skeleton.c
+++ week1_LinkedList/skeleton/LinkedList_skeleton.c
@@ -27,24 +27,23 @@ void PrintList(List *plist)
 }
 
 // 4
-int FindNode(List *plist, ElementType key_element)
+Node* FindNode(List *plist, ElementType key_element)
 {
     // key값을 element로 하는 node를 찾는다
-    // 해당 node가 존재하면 plist->cur 포인터가 찾은 node를 가리키도록 하고, 1을 return 한다.
-    // 해당 node가 존재하지 않으면, 0을 return한다.
+    // 해당하는 node가 없으면 NULL을 return하도록 한다.
     return 0;
 }
 
 // 5
 int FindPrevNode(List *plist, ElementType key)
 {
-    // key값을 element로 하는 node의 앞의 node를 찾는다.
+    // key값을 element로 하는 node의 previous node를 찾는다.
     return 0;
 }
 
 // 2
 int IsEmpty(List *plist)
 {
-    // list가 비어있으면 1을, 비어있지 않으면 0을 return 한다.
+    // list가 비어있으면 TRUE를, 비어있지 않으면 FALSE을 return 한다.
     return 0;
 }
