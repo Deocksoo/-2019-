62d974785d057f9bdaf8c5037163a02fe92404e4
wbluke <pwb0127@naver.com>
2019-04-20 12:34:45 +0900

    [ADD] week2 Stack 스켈레톤 코드 구현

--- week2_Stack/skeleton/main_lab4_skeleton.c
+++ week2_Stack/skeleton/main_lab4_skeleton.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "stack.h"
+#include "Stack.h"
 
 #define MAX_BUFFER_SIZE  100
 
@@ -22,12 +22,55 @@ int ReturnPriority(char op)
     }
 }
 
+int comparePriorityOfOperator(char op1, char op2) {
+    if (op1 == '(') {
+        return -1;
+    }
+
+    int priorityOfOp1 = ReturnPriority(op1);
+    int priorityOfOp2 = ReturnPriority(op2);
+
+    if (priorityOfOp1 > priorityOfOp2) {
+        return 1;
+    }
+    else if (priorityOfOp1 == priorityOfOp2) {
+        return 0;
+    }
+    else {
+        return -1;
+    }
+}
+
 char *InfixToPostfix(char *infix)
 {
     Stack stack;
     StackInit(&stack);
     char *postfix = (char *)malloc(sizeof(char) * (strlen(infix) + 1));  // length + 1 for end zero
     memset(postfix, 0, sizeof(char) * (strlen(infix) + 1));
+
+    int lastIdx = 0;
+    for(int i = 0; i < strlen(infix); i++) {
+        if( infix[i] >= '0' && infix[i] <= '9' ) {
+            postfix[lastIdx++] = infix[i];
+            continue;
+        }
+
+        if (infix[i] == ')') {
+            while(SPeek(&stack) != '(') {
+                postfix[lastIdx++] = SPop(&stack);
+            }
+            SPop(&stack);
+            continue;
+        }
+
+        while(SIsEmpty(&stack) == FALSE && comparePriorityOfOperator(SPeek(&stack), infix[i]) >= 0) {
+            postfix[lastIdx++] = SPop(&stack);
+        }
+        if (SIsEmpty(&stack) == TRUE || comparePriorityOfOperator(SPeek(&stack), infix[i]) == -1){
+            SPush(&stack, infix[i]);
+            continue;            
+        }
+    }
 
     return postfix;
 }
