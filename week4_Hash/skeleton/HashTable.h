b970be5e7aae5b48f804a60a9eabcb334c525f24
deocksoo <dusol92@snu.ac.kr>
2019-05-18 08:02:07 +0900

    [ADD] HashTable 스켈레톤 코드를 추가합니다

---
+++ week4_Hash/skeleton/HashTable.h
@@ -0,0 +1,23 @@
+#ifndef __HASH_TABLE__
+#define __HASH_TABLE__
+
+#include "LinkedList.h"
+#include "Slot.h"
+
+typedef int HashFunc(Value value, float constant_a, int m);
+
+typedef struct _table
+{
+    int hash_size;
+    List *tbl;
+    HashFunc *hf;
+    float constant_a;
+} Table;
+
+void TBLInit(Table *ptable, int hash_size, HashFunc *f, float constant_a);
+void TBLInsert(Table *ptable, Key key, Value value);
+void TBLDelete(Table *ptable, Key key);
+Slot TBLFind(Table *ptable, Key key);
+void TBLPrint(Table *ptable);
+
+#endif
---
+++ week4_Hash/skeleton/HashTable_skeleton.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "HashTable.h"
+#include "LinkedList.h"
+
+//1
+void TBLInit(Table *ptable, int hash_size, HashFunc *f, float constant_a) {
+
+}
+
+// 3
+void TBLInsert(Table *ptable, Key key, Value value) {
+
+}
+
+// 4
+void TBLDelete(Table *ptable, Key key) {
+
+}
+
+// 5 
+Slot TBLFind(Table *ptable, Key key) {
+    Slot slot;
+
+    return slot;
+}
+
+// 2
+void TBLPrint(Table *ptable) {
+
+}
